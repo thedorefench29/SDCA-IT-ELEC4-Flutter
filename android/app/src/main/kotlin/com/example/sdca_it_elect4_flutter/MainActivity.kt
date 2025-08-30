7b4e8d033b2e28406c9c1abe08b10d4d0bcb5521
Audrey Maven Concepcion <audreymaven9742@gmail.com>
2025-08-30 18:24:08 +0800

    Initial Flutter Code

---
+++ android/app/src/main/kotlin/com/example/sdca_it_elect4_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sdca_it_elect4_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Scaffold homepage = Scaffold(
+      appBar: AppBar(title: Text('Homepage'))
+    );
+
+    return MaterialApp(
+      home: Text('Hello World')
+      home: homepage,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.white,
+        )
+      )
+    );
+  }
+}
